c7b4e16aedb14b04247cd8c66767976e439686c6
Edward-Jackson-ONS <92307552+Edward-Jackson-ONS@users.noreply.github.com>
2024-08-15 13:07:45 +0100

    3 setup environment and ci (#4)
    * chore: project environment
    
    * ci: initial setup
    
    * test: placeholder test to allow CI
    
    * ci: temporarily suspend until first code push

---
+++ src/parliai_public/__init__.py
@@ -0,0 +1,7 @@
+"""Using LLMs to capture coverage of organisations, people or themes in UK political debate."""
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "__version__",
+]
---
+++ tests/test_placeholder.py
@@ -0,0 +1,7 @@
+"""Example test to facilitate CI operation."""
+
+
+def test_temp():
+    """Test message."""
+    msg = "I am a test"
+    assert msg == "I am a test"
